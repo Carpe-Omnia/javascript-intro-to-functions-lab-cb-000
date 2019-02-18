f4a107ee917eead1dba3652e630f1f16cbf92581
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-18 18:23:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(string) {}
+function logShout(string) {
+  console.log( string.toUpperCase() )
+}
 
 function logWhisper(string){}
 function sayHiToGrandma(string){}
